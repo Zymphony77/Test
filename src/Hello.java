63149bb1177e9d9770ef6fad195d2153521036e9
Chawit Sakulyuenyong <Chawit.Sky@hotmail.com>
2017-08-26 16:03:59 +0700

    Added source code Hello.java

---
+++ src/Hello.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Hello {
+	public static void main(String[] args) {
+		System.out.println("Hello");
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		System.out.println((int)Math.pow(n, 2));
+	}
+}
